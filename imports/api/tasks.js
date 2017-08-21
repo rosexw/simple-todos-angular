55205561a333b64c2e34c78ada149af5c97bb7a6
Rose Wan <rose.wan@me.com>
2017-08-20 22:57:32 -0400

    ch10: publish and subscribe. Private methods added to user accounts, so no other users can delete another user's tasks

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -1,9 +1,24 @@
-
 import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 import { check } from 'meteor/check';
 
 export const Tasks = new Mongo.Collection('tasks');
+
+if (Meteor.isServer) {
+  // This code only runs on the server
+  // Only publish tasks that are public or belong to the current user
+  Meteor.publish('tasks', function tasksPublication() {
+    return Tasks.find({
+      $or: [{
+        private: {
+          $ne: true
+        }
+      }, {
+        owner: this.userId
+      }, ],
+    });
+  });
+}
 
 Meteor.methods({
   'tasks.insert' (text) {
@@ -24,11 +39,23 @@ Meteor.methods({
   'tasks.remove' (taskId) {
     check(taskId, String);
 
+    const task = Tasks.findOne(taskId);
+    if (task.private && task.owner !== Meteor.userId()) {
+      // If the task is private, make sure only the owner can delete it
+      throw new Meteor.Error('not-authorized');
+    }
+
     Tasks.remove(taskId);
   },
   'tasks.setChecked' (taskId, setChecked) {
     check(taskId, String);
     check(setChecked, Boolean);
+
+    const task = Tasks.findOne(taskId);
+    if (task.private && task.owner !== Meteor.userId()) {
+      // If the task is private, make sure only the owner can check it off
+      throw new Meteor.Error('not-authorized');
+    }
 
     Tasks.update(taskId, {
       $set: {
@@ -36,3 +63,21 @@ Meteor.methods({
       }
     });
   },
+  'tasks.setPrivate' (taskId, setToPrivate) {
+    check(taskId, String);
+    check(setToPrivate, Boolean);
+
+    const task = Tasks.findOne(taskId);
+
+    // Make sure only the task owner can make a task private
+    if (task.owner !== Meteor.userId()) {
+      throw new Meteor.Error('not-authorized');
+    }
+
+    Tasks.update(taskId, {
+      $set: {
+        private: setToPrivate
+      }
+    });
+  },
+});
